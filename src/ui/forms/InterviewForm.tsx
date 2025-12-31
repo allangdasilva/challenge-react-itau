0c3ebd3866c871d923c724c7b926856b4de92cd5
Allan Silva <allandasilva33@outlook.com>
2025-12-30 22:42:06 -0300

    Estilizar o componente InterviewForm

--- src/ui/forms/InterviewForm.tsx
+++ src/ui/forms/InterviewForm.tsx
@@ -6,42 +6,50 @@ import Button from "../Button";
 
 const InterviewForm = () => {
   return (
-    <form action="">
-      <h2>_Trabalhe Conosco</h2>
+    <form className="m-auto p-8 space-y-6 rounded-2xl max-w-lg bg-primary">
+      <h2 className="font-title text-light">_Trabalhe Conosco</h2>
 
-      <InputField
-        label="Nome"
-        type="text"
-        name="first-name"
-        placeholder="Nome"
-      />
-      <InputField
-        label="Sobrenome"
-        type="text"
-        name="last-name"
-        placeholder="Sobrenome"
-      />
+      <div className="space-y-4">
+        <div className="grid gap-4 sm:grid-cols-2">
+          <InputField
+            label="Nome"
+            type="text"
+            name="first-name"
+            placeholder="Nome"
+          />
+          <InputField
+            label="Sobrenome"
+            type="text"
+            name="last-name"
+            placeholder="Sobrenome"
+          />
+        </div>
 
-      <SelectField
-        label="Selecione a vaga"
-        name="select-vacancy"
-        options={["Backend", "Frontend", "QA"]}
-        placeholder="Selecione a vaga"
-      />
-      <SelectField
-        label="Selecione um nível"
-        name="select-level"
-        options={["Jr.", "Pl.", "Sn."]}
-        placeholder="Nv."
-      />
+        <div className="grid gap-4 sm:grid-cols-[1.5fr_0.5fr]">
+          <SelectField
+            label="Selecione a vaga"
+            name="select-vacancy"
+            options={["Backend", "Frontend", "QA"]}
+            placeholder="Selecione a vaga"
+          />
+          <SelectField
+            label="Selecione um nível"
+            name="select-level"
+            options={["Jr.", "Pl.", "Sn."]}
+            placeholder="Nv."
+          />
+        </div>
 
-      <TextareaField
-        label="Fale um pouco sobre você"
-        name="about-yourself"
-        placeholder="Fale um pouco sobre você"
-      />
+        <TextareaField
+          label="Fale um pouco sobre você"
+          name="about-yourself"
+          placeholder="Fale um pouco sobre você"
+        />
+      </div>
 
-      <Button type="submit">Enviar</Button>
+      <div className="grid justify-center">
+        <Button type="submit">Enviar</Button>
+      </div>
     </form>
   );
 };
