e0227b3f3f59e97301ff5b9eb4f539f72069107a
Allan Silva <allandasilva33@outlook.com>
2026-01-02 20:35:34 -0300

    Adicionar reatividade aos componentes do InterviewForm

--- src/ui/form-fields/InputField.tsx
+++ src/ui/form-fields/InputField.tsx
@@ -2,20 +2,11 @@ import React from "react";
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  type: string;
   name: string;
-  placeholder: string;
   rounded?: string;
 }
 
-const InputField = ({
-  label,
-  type,
-  name,
-  placeholder,
-  rounded,
-  ...props
-}: InputFieldProps) => {
+const InputField = ({ label, name, rounded, ...props }: InputFieldProps) => {
   return (
     <div>
       <label className="sr-only" htmlFor={name}>
@@ -23,10 +14,8 @@ const InputField = ({
       </label>
       <input
         {...props}
-        type={type}
         id={name}
         name={name}
-        placeholder={placeholder}
         className={`${rounded} w-full p-4 outline-0 font-body text-dark placeholder:font-body placeholder:text-placeholder border-b-2 border-disabled focus:border-secondary focus:outline-none bg-light`}
       />
     </div>
--- src/ui/forms/InterviewForm.tsx
+++ src/ui/forms/InterviewForm.tsx
@@ -5,8 +5,31 @@ import TextareaField from "../form-fields/TextareaField";
 import Button from "../Button";
 
 const InterviewForm = () => {
+  const [field, setField] = React.useState({
+    firstName: "",
+    lastName: "",
+    vacancy: "",
+    level: "",
+    message: "",
+  });
+
+  function handleChange<
+    T extends HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+  >(e: React.ChangeEvent<T>) {
+    const { name, value } = e.target;
+    setField({ ...field, [name]: value });
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(field);
+  }
+
   return (
-    <form className="w-full m-auto p-8 space-y-6 rounded-2xl max-w-lg bg-primary">
+    <form
+      onSubmit={handleSubmit}
+      className="w-full m-auto p-8 space-y-6 rounded-2xl max-w-lg bg-primary"
+    >
       <h2 className="font-title text-light">_Trabalhe Conosco</h2>
 
       <div className="space-y-4">
@@ -14,43 +37,51 @@ const InterviewForm = () => {
           <InputField
             label="Nome"
             type="text"
-            name="first-name"
+            name="firstName"
             placeholder="Nome"
             rounded="max-sm:rounded-t-lg sm:rounded-tl-lg"
+            value={field.firstName}
+            onChange={(e) => handleChange<HTMLInputElement>(e)}
           />
           <InputField
             label="Sobrenome"
             type="text"
-            name="last-name"
+            name="lastName"
             placeholder="Sobrenome"
             rounded="sm:rounded-tr-lg"
+            value={field.lastName}
+            onChange={(e) => handleChange<HTMLInputElement>(e)}
           />
         </div>
 
         <div className="grid gap-4 sm:grid-cols-[1.5fr_0.5fr]">
           <SelectField
             label="Selecione a vaga"
-            name="select-vacancy"
+            name="vacancy"
             options={["Backend", "Frontend", "QA"]}
             placeholder="Selecione a vaga"
             required
+            onChange={(e) => handleChange<HTMLSelectElement>(e)}
           />
           <SelectField
             label="Selecione um nível"
-            name="select-level"
+            name="level"
             options={["Jr.", "Pl.", "Sn."]}
             placeholder="Nv."
             required
+            onChange={(e) => handleChange<HTMLSelectElement>(e)}
           />
         </div>
 
         <TextareaField
           label="Fale um pouco sobre você"
-          name="about-yourself"
+          name="message"
           placeholder="Fale um pouco sobre você"
           rounded="rounded-b-lg"
           rows={4}
           maxLength={255}
+          value={field.message}
+          onChange={(e) => handleChange<HTMLTextAreaElement>(e)}
         />
       </div>
 
